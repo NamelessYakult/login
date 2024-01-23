d35bff6f3d67b138bd4174e650a0f7caa371de5a
NamelessYakult <prakhardixitrocks@gmail.com>
2024-01-23 17:23:24 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const pasth = require('path');
+const bcrypt = require('bcrypt');
+const collection = require("./config");
+
+const app = express();
+
+//note for Vedant - Do not touch, converts data into .json
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+
+//note for Vedant - EJS used as a view-engine
+app.set('view engine', 'ejs');
+
+//note for Vedant - static file
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+app.post("/signup", async (req, res) => {
+
+    const data = {
+        name: req.body.username,
+        password: req.body.password
+    };
+
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+
+});
+
+const port = 5000;
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+});
